095a596c6ab74a86963787e474919ae6c354392e
Kota Ishida <kota.ishida@di-v.co.jp>
2023-09-26 20:56:11 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ApplicationRecord
-  t.string  :content
-  t.references :room, null: false, foreign_key: true
-  t.references :user, null: false, foreign_key: true
-
   belongs_to :room
   belongs_to :user
+
+  validates :content, presence: true
 end
